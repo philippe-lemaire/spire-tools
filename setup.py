4694f82d8464c2ecc1c244a6c56182e2b70c329f
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-08-23 18:29:54 +0200

    added description.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='spire-tools',
       version="1.0",
-      description="Project Description",
+      description="Web tool to run the Spire RPG",
       packages=find_packages(),
       install_requires=requirements,
       test_suite='tests',
