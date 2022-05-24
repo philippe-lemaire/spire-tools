57e72e01791cd50644ffd7a0b9f7f4c65236d9ed
Philippe Lemaire <philippe.lemaire@protonmail.com>
2022-05-24 14:12:20 +0200

    added difficulty handling

--- spire-tools/roller.py
+++ spire-tools/roller.py
@@ -1,18 +1,46 @@
 import random
 
 
-def roll(dice):
+def roll(dice, difficulty=0):
     """Rolls a dice number of d10s, keeps the best one, and returns the dice pool for display and the text result."""
+    offset = 0
+    dice -= difficulty
+    
+    if dice < 1:
+        dice = 1
+        offset = 2
+        print(f"Difficulty over dice pool, rolling 1 die but the result will be degraded by 2 steps")
+        
     pool = [random.randint(1, 10) for die in range(dice)]
     best = max(pool)
+    
+    result_ranges = [
+        "Critical failure. Fail, and take double stress.",
+        "Failure. Fail, and take stess.",
+        "Success at a cost. Succeed, but take stress.",
+        "Success. Succeed, and take no stress.",
+        "Critical Success. Succeed dramatically, and increase outgoing stress dice by 1 step.",
+    ]
+    
     if best == 1:
-        res = "Critical failure. Fail, and take double stress."
+        idx = 0
     elif best in range(1, 6):
-        res = "Failure. Fail, and take stess."
+        idx = 1
     elif best in range(6, 8):
-        res = "Success at a cost. Succeed, but take stress."
+        idx = 2
     elif best in range(8, 10):
-        res = "Success. Succeed, and take no stress."
+        idx = 3
     elif best == 10:
-        res = "Critical Success. Succeed dramatically, and increase outgoing stress dice by 1 step."
+        idx = 4
+    
+    print(f"{idx=}")
+    idx -= offset
+    
+    
+    res = result_ranges[max(idx, 0)]
+    
     return pool, res
+
+
+if __name__ == '__main__':
+    print(roll(dice=5, difficulty=3))
